b80312d1182aa64a49ac0de8ca0b7b8e8de0601e
zelinskyiak24-maker <zelinskyi_ak24@nuwm.edu.ua>
2026-01-13 23:53:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,73 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
-namespace LabWork
+namespace SystemDrawingGraph
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    public class Form1 : Form
     {
-        static void Main(string[] args)
+        private const double XMin = -1;
+        private const double XMax = 2.3;
+        private const double Step = 0.7;
+
+        public Form1()
         {
-            
-            Console.WriteLine("Hello World!");
+            Text = "System.Drawing Graph";
+            Width = 800;
+            Height = 600;
+            Paint += Form1_Paint;
+            Resize += (s, e) => Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGraph(e.Graphics);
+        }
+
+        private double CalculateY(double x)
+        {
+            return (Math.Exp(2 * x) - 8) / (x + 3);
+        }
+
+        private void DrawGraph(Graphics g)
+        {
+            g.Clear(Color.White);
+            int width = ClientSize.Width;
+            int height = ClientSize.Height;
+
+            List<PointF> points = new();
+            double yMin = double.MaxValue, yMax = double.MinValue;
+
+            for (double x = XMin; x <= XMax; x += Step)
+            {
+                double y = CalculateY(x);
+                yMin = Math.Min(yMin, y);
+                yMax = Math.Max(yMax, y);
+            }
+
+            double scaleX = width / (XMax - XMin);
+            double scaleY = height / (yMax - yMin);
+
+            for (double x = XMin; x <= XMax; x += Step)
+            {
+                double y = CalculateY(x);
+                float sx = (float)((x - XMin) * scaleX);
+                float sy = (float)(height - (y - yMin) * scaleY);
+                points.Add(new PointF(sx, sy));
+            }
+
+            using Pen axis = new(Color.Black, 2);
+            using Pen graph = new(Color.Blue, 2);
+
+            float zeroY = (float)(height - (0 - yMin) * scaleY);
+            float zeroX = (float)((0 - XMin) * scaleX);
+
+            g.DrawLine(axis, 0, zeroY, width, zeroY);
+            g.DrawLine(axis, zeroX, 0, zeroX, height);
+
+            for (int i = 0; i < points.Count - 1; i++)
+                g.DrawLine(graph, points[i], points[i + 1]);
         }
     }
 }
